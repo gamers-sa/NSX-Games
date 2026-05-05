6216f69569e267a56efe6489b81e7eaaa7f0eade
gamers-sa <dark_crimson@live.com>
2026-05-05 05:26:46 -0600

    Update 5143-4d5c3f0fb60b9772.js

--- js/5143-4d5c3f0fb60b9772.js
+++ js/5143-4d5c3f0fb60b9772.js
@@ -1,7 +1,1 @@
-var link = document.createElement("link");
-link.rel = "stylesheet";
-link.type = "text/css";
-link.href = "https://cdn.jsdelivr.net/gh/gamers-sa/new/index.css";
-if (window.location.hostname === "gamers-sa.github.io") {
-    document.getElementsByTagName("head")[0].appendChild(link);
-}
+
